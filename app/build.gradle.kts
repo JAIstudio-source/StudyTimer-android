bb6e1e09bac1a94a224a27eb1b5800bd0264f7d4
JAIstudio-source <defender9817@gmail.com>
2026-08-10 23:01:34 +0530

    Bump version to 2.5.0 and release signed APK with Google authentication & cloud sync features

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         minSdk = 28
         targetSdk = 36
         
-        versionCode = 13
-        versionName = "2.0.1"
+        versionCode = 14
+        versionName = "2.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
